fb26143d6647bd369c03a72c4e8d8c746db8e5f8
Oleksii Bohomaz <a@bos-info.com>
2022-09-09 07:58:47 +0300

    Initial commit

--- dbf.go
+++ dbf.go
@@ -2,9 +2,7 @@ package dbf
 
 // reference implementation:
 //     http://dbf.berlios.de/
-
 // test data: http://abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/2923.0.30.0012006?OpenDocument
-
 // a dbf.Reader should have some metadata, and a Read() method that returns
 // table rows, one at a time
 
@@ -12,6 +10,7 @@ import (
 	"bufio"
 	"encoding/binary"
 	"fmt"
+	"golang.org/x/text/encoding/charmap"
 	"io"
 	"strconv"
 	"strings"
@@ -19,23 +18,24 @@ import (
 )
 
 type Reader struct {
-	r                io.ReadSeeker
+	rs               io.ReadSeeker
 	year, month, day int
-	Length           int // number of records
+	length           int // number of records
 	fields           []Field
-	headerlen        uint16 // in bytes
-	recordlen        uint16 // length of each record, in bytes
+	headerLength     uint16 // in bytes
+	recordLength     uint16 // length of each record, in bytes
 	sync.Mutex
 }
 
 type header struct {
 	// documented at: http://www.dbase.com/knowledgebase/int/db7_file_fmt.htm
-	Version    byte
-	Year       uint8 // stored as offset from (decimal) 1900
-	Month, Day uint8
-	Nrec       uint32
-	Headerlen  uint16 // in bytes
-	Recordlen  uint16 // length of each record, in bytes
+	version      byte
+	year         uint8 // stored as offset from (decimal) 1900
+	month        uint8
+	day          uint8
+	numberRecord uint32
+	headerLength uint16 // in bytes
+	recordLength uint16 // length of each record, in bytes
 }
 
 func NewReader(r io.ReadSeeker) (*Reader, error) {
@@ -46,8 +46,8 @@ func NewReader(r io.ReadSeeker) (*Reader, error) {
 	err := binary.Read(r, binary.LittleEndian, &h)
 	if err != nil {
 		return nil, err
-	} else if h.Version != 0x03 {
-		return nil, fmt.Errorf("unexepected file version: %d\n", h.Version)
+	} else if h.version != 0x03 {
+		return nil, fmt.Errorf("unexepected file version: %day\n", h.version)
 	}
 
 	var fields []Field
@@ -55,9 +55,12 @@ func NewReader(r io.ReadSeeker) (*Reader, error) {
 		return nil, err
 	}
 	var offset uint16
-	for offset = 0x20; offset < h.Headerlen-1; offset += 32 {
+	for offset = 0x20; offset < h.headerLength-1; offset += 32 {
 		f := Field{}
-		binary.Read(r, binary.LittleEndian, &f)
+		errBin := binary.Read(r, binary.LittleEndian, &f)
+		if errBin != nil {
+			fmt.Println("binary.Read failed:", errBin)
+		}
 		if err = f.validate(); err != nil {
 			return nil, err
 		}
@@ -68,12 +71,12 @@ func NewReader(r io.ReadSeeker) (*Reader, error) {
 	if eoh, err := br.ReadByte(); err != nil {
 		return nil, err
 	} else if eoh != 0x0D {
-		return nil, fmt.Errorf("Header was supposed to be %d bytes long, but found byte %#x at that offset instead of expected byte 0x0D\n", h.Headerlen, eoh)
+		return nil, fmt.Errorf("Header was supposed to be %d bytes long, but found byte %#x at that offset instead of expected byte 0x0D\n", h.headerLength, eoh)
 	}
 
-	return &Reader{r, 1900 + int(h.Year),
-		int(h.Month), int(h.Day), int(h.Nrec), fields,
-		h.Headerlen, h.Recordlen, *new(sync.Mutex)}, nil
+	return &Reader{r, 1900 + int(h.year),
+		int(h.month), int(h.day), int(h.numberRecord), fields,
+		h.headerLength, h.recordLength, *new(sync.Mutex)}, nil
 }
 
 func (r *Reader) ModDate() (int, int, int) {
@@ -96,7 +99,7 @@ func (f *Field) validate() error {
 	case 'C', 'N', 'F':
 		return nil
 	}
-	return fmt.Errorf("Sorry, dbf library doesn't recognize field type '%c'", f.Type)
+	return fmt.Errorf("sorry, dbf library doesn't recognize field type '%c'", f.Type)
 }
 
 type Field struct {
@@ -111,29 +114,27 @@ type Field struct {
 	_ [14]byte
 }
 
-// http://play.golang.org/p/-CUbdWc6zz
 type Record map[string]interface{}
 
 func (r *Reader) Read(i uint16) (rec Record, err error) {
 	r.Lock()
 	defer r.Unlock()
-
-	offset := int64(r.headerlen + r.recordlen*i)
-	r.r.Seek(offset, 0)
+	offset := int64(r.headerLength) + int64(r.recordLength)*int64(i)
+	r.rs.Seek(offset, 0)
 
 	var deleted byte
-	if err = binary.Read(r.r, binary.LittleEndian, &deleted); err != nil {
+	if err = binary.Read(r.rs, binary.LittleEndian, &deleted); err != nil {
 		return nil, err
 	} else if deleted == '*' {
 		return nil, fmt.Errorf("record %d is deleted", i)
 	} else if deleted != ' ' {
-		return nil, fmt.Errorf("record %d contained an unexpected value in the deleted flag: %h", i, deleted)
+		return nil, fmt.Errorf("record %d contained an unexpected value in the deleted flag: %v", i, deleted)
 	}
 
 	rec = make(Record)
 	for i, f := range r.fields {
 		buf := make([]byte, f.Len)
-		if err = binary.Read(r.r, binary.LittleEndian, &buf); err != nil {
+		if err = binary.Read(r.rs, binary.LittleEndian, &buf); err != nil {
 			return nil, err
 		}
 
@@ -149,11 +150,18 @@ func (r *Reader) Read(i uint16) (rec Record, err error) {
 			}
 		case 'N':
 			if len(fieldVal) == 0 {
-				rec[fieldName] = int(0)
+				rec[fieldName] = 0
 			} else if f.DecimalPlaces > 0 {
 				rec[fieldName], err = strconv.ParseFloat(fieldVal, 64)
 			} else {
 				rec[fieldName], err = strconv.Atoi(fieldVal)
+			}
+		case 'C':
+			if len(fieldVal) == 0 {
+				rec[fieldName] = ""
+			} else {
+				decoder := charmap.Windows1251.NewDecoder()
+				rec[fieldName], err = decoder.String(fieldVal)
 			}
 		default:
 			rec[fieldName] = fieldVal
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bos-info/dbfcp1251
+
+go 1.19
+
+require golang.org/x/text v0.3.7
